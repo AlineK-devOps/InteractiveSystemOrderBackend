ce821d60c39d8d851dba150c46176e5c0d787414
Aline Korshunova <aline.korshunova00@gmail.com>
2022-05-14 12:22:52 +0700

    Initial commit

---
+++ src/test/java/ru/nstu/sysordbackend/SysordbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nstu.sysordbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SysordbackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
